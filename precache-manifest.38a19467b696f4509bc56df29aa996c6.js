04b4e6ce2a41bff0285f1305f3a34d296f14e774
soumya jain <soumya.11mg@gmail.com>
2020-05-20 19:31:43 +0530

    Updates

---
+++ precache-manifest.38a19467b696f4509bc56df29aa996c6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "03ac117158aeb0e2883f9139feb3f8d0",
+    "url": "/bluestacks_assignment/index.html"
+  },
+  {
+    "revision": "5936b3d0097f350fdfd3",
+    "url": "/bluestacks_assignment/static/css/2.abe18057.chunk.css"
+  },
+  {
+    "revision": "1deec8946dec9452af7d",
+    "url": "/bluestacks_assignment/static/css/main.661f3518.chunk.css"
+  },
+  {
+    "revision": "5936b3d0097f350fdfd3",
+    "url": "/bluestacks_assignment/static/js/2.5f72cd22.chunk.js"
+  },
+  {
+    "revision": "c5efcd60b546baed2642f72434043964",
+    "url": "/bluestacks_assignment/static/js/2.5f72cd22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1deec8946dec9452af7d",
+    "url": "/bluestacks_assignment/static/js/main.c706c1c9.chunk.js"
+  },
+  {
+    "revision": "ed6f4b0b4aabbd7b9be9",
+    "url": "/bluestacks_assignment/static/js/runtime-main.e07e4853.js"
+  },
+  {
+    "revision": "5e98861f6ef1ce225121abfb3f418166",
+    "url": "/bluestacks_assignment/static/media/getFetch.5e98861f.cjs"
+  }
+]);
